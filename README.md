65788853549b32bfd72cf8c2dd301ec2e140ff16
Latif <mdsabdullotif@gmail.com>
2025-11-03 17:06:12 +0600

    add all file

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # FastAPI_for_Backend
+
+## haw to install FastAPI
+
+* pip install "fastapi[standard]"
+
+## install postgresql
+
+* pip install psycopg2
---
+++ main.py
@@ -0,0 +1,56 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, HttpUrl
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
+
+app = FastAPI()
+
+# Define request body schema
+class Course(BaseModel):
+    name: str
+    instructor: str
+    duration: float
+    website: HttpUrl
+
+# Database connection
+while True:
+    try:
+        conn = psycopg2.connect(
+            host='localhost',
+            database='aiquest',
+            user='postgres',
+            password='4321',
+            cursor_factory=RealDictCursor
+        )
+        cursor = conn.cursor()
+        print("Successfully connected to database")
+        break
+    except Exception as error:
+        print("Database connection failed")
+        print("Error:", error)
+        time.sleep(2)
+
+
+@app.post("/post")
+def create_post(post: Course):
+    cursor.execute(""" INSERT INTO course(name, instructor, duration, website) VALUES(%s,%s,%s,%s)""", (post.name, post.instructor, post.duration, post.website))
+    new_post = cursor.fetchone()
+    conn.commit()
+    return {"data": new_post}
+
+@app.get("/course")
+def studymart():
+    return {"backend": "for self"}
+
+@app.get("/")
+def aiquest():
+    cursor.execute("""SELECT * FROM course""")
+    data = cursor.fetchall()
+    return {"data":data}
+
+
+
+@app.get("/fastapi/api")
+def fastapi():
+    return {"backend": "for self"}
